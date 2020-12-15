7da1186c5f795a6505c29b2eba76cb0919e12b08
MArsalanAI3 <marsalanai3@gmail.com>
2020-12-15 05:32:10 +0500

    Added Theme-UI

--- components/packages/src/pages/index.js
+++ components/packages/src/pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Container, Button, Heading,Flex } from 'theme-ui';
 
+export default props => (
+    <Container>
+        <Flex sx={{flexDirection:"column",padding:3}}>
 
-export default props=>(
-    <div>
-        Hello World!
-    </div>
+            <Heading as="h1">Todos App</Heading>
+            <Button sx={{marginTop:3,color:"Black",fontWeight:"bold",fontSize:22}}
+             onClick={()=>alert("Hello World!")}   
+            >
+                Submited
+                </Button>
+        </Flex>
+    </Container>
 );
---
+++ components/packages/wrap-root-element.js
@@ -0,0 +1,13 @@
+const React = require("react");
+const { ThemeProvider }=require( 'theme-ui');
+const {Deep} = require('@theme-ui/presets');
+
+
+const newTheme={
+  ...Deep,
+  sizes:{container: 1024}
+};
+
+module.exports= ({element}) => (
+  <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
+);
